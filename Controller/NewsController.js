727cd51bc74d765d3027f65b4c5b5e51c07019d4
xuanhei <2441415772@qq.com>
2020-09-10 22:55:04 +0800

    修复@GetMapping("")之类的请求注解返回字符串或者自定义类型失败

---
+++ Controller/NewsController.js
@@ -0,0 +1,21 @@
+/**
+ * Greated By xuanhei on 2020/9/10
+ **/
+
+
+const {Api,Controller,PostMapping,GetMapping,DeleteMapping,PutMapping,FilePostMapping} = require('../Annotation/HttpAnnotion');
+
+
+/**
+ * 首页api
+ */
+@Api("/News")
+@Controller
+class IndexController{
+
+    @GetMapping("/index")
+    getIndex(){
+        return "你好";
+    }
+
+}
